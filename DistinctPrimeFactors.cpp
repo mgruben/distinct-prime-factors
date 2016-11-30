6bc7738cd10b026f463a7344f66a3ddc46c96b7e
mgruben <GrubenM@gmail.com>
2016-11-30 12:00:56 -0600

    Remove unused helper function

--- DistinctPrimeFactors.cpp
+++ DistinctPrimeFactors.cpp
@@ -3,25 +3,6 @@
 using namespace std;
 
 /**
- * Given an integer n, returns the number of prime factors which that
- * number has.
- * Ex. 14 -> 2, 15 -> 2.
- * Ex. 644 -> 3, 645 -> 3, 646 -> 3.
- */
-int numFactors(int n) {
-    if (n <= 3) return 1;
-    if (n % 2 == 0) return 1 + numFactors(n / 2);
-    if (n % 3 == 0) return 1 + numFactors(n / 3);
-    int i = 5;
-    while (i * i <= n) {
-        if (n % i == 0) return 1 + numFactors(n / i);
-        if (n % (i + 2) == 0) return 1 + numFactors(n / (i + 2));
-        i += 6;
-    }
-    return true;
-}
-
-/**
  * Given an integer n and a reference to a vector v,
  * push factors of n onto v and recurse.
  * 
